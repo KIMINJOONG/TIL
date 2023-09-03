66dec1d3f2d9c810baed069b9996174262d95f92
KimInJoong <ijboym216@gmail.com>
2023-09-03 19:34:39 +0900

    1. 명명된(named) 볼륨으로 구조하기

--- Docker&Kubernetes/data-volumes-01-starting-setup/server.js
+++ Docker&Kubernetes/data-volumes-01-starting-setup/server.js
@@ -36,9 +36,9 @@ app.post("/create", async (req, res) => {
     if (exists) {
       res.redirect("/exists");
     } else {
-      await fs.rename(tempFilePath, finalFilePath);
-      // await fs.copyFile(tempFilePath, finalFilePath);
-      // await fs.unlink(tempFilePath);
+      // await fs.rename(tempFilePath, finalFilePath);
+      await fs.copyFile(tempFilePath, finalFilePath);
+      await fs.unlink(tempFilePath);
       res.redirect("/");
     }
   });
