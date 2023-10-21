e35abcb3bbfd4ef601dbc8123b28efefd31825e6
KimInJoong <ijboym216@gmail.com>
2023-10-21 21:07:28 +0900

    일부 Docker Compose 서비스만 구동하기

--- node/RESTDemo/index.js
+++ node/RESTDemo/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const { v4: uuid } = require("uuid");
+const methodOverride = require("method-override");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -12,22 +15,22 @@ app.set("view engine", "ejs");
 // Our fake database:
 let comments = [
   {
-    // id: 1,
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny!",
   },
   {
-    // id: 2,
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
-    // id: 3,
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
-    // id: 4,
+    id: uuid(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
@@ -42,9 +45,36 @@ app.get("/comments/new", (req, res) => {
 });
 
 app.post("/comments", (req, res) => {
-  const { comment, username } = res.body;
-  comments.push({ comment, username });
-  res.redirect("comments/show", { comment: { comment, username } });
+  const { comment, username } = req.body;
+  const id = uuid();
+  comments.push({ comment, username, id });
+  res.redirect("/comments");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((item) => item.id === id);
+  res.render("comments/show", { comment });
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const foundComment = comments.find((item) => item.id === id);
+  const newComment = req.body.comment;
+  newComment.comment = foundComment;
+  res.redirect("comments");
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((item) => item.id === id);
+  res.render("comments/edit", { comment });
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((item) => item.id !== id);
+  res.redirect("/comments");
 });
 
 app.listen(3000, () => {
